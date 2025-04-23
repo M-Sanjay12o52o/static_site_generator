5e621e9f185d3ba4a085f12f4a989d3ca999f096
M-Sanjay12o52o <msnjy12o52o@gmail.com>
2025-04-23 15:32:19 +0530

    moved build from public to docs

---
+++ build.sh
@@ -0,0 +1,1 @@
+src/main.py "/static_site_generator/"
--- src/main.py
+++ src/main.py
@@ -1,14 +1,18 @@
 import shutil
 import os
+import sys
 
 # from generate_page import generate_page
 from copystatic import copy_files_recursive
 from generate_page_recursive_fn import generate_pages_recursive
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+# dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
+
+basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
 
 
 def main():
